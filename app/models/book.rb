685dd74bbd026aad3ccfafcce67e4331fc1f6618
Ryan Sprott <rdsprott@gmail.com>
2018-05-07 18:31:59 -0400

    Added scopes to Book model.

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,4 +2,9 @@ class Book < ApplicationRecord
   belongs_to :format
   has_and_belongs_to_many :authors
   validates_presence_of :title, :release_date, :authors
+  scope :published_2018, -> { 
+    where('release_date > ?', DateTime.new(2017, 12, 31))
+    .where('release_date < ?', DateTime.new(2018, 12, 31))
+  }
+  scope :by_format, -> (format) { where(format_id: format.id) }
 end
