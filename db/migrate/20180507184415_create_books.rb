adf83a2005c48bb613c4d28e894ee9f8acc817a9
Ryan Sprott <rdsprott@gmail.com>
2018-05-07 14:45:24 -0400

    Created Books table.

--- db/migrate/20180507184415_create_books.rb
+++ db/migrate/20180507184415_create_books.rb
@@ -1,6 +1,9 @@
 class CreateBooks < ActiveRecord::Migration[5.1]
   def change
     create_table :books do |t|
+      t.string :title
+      t.datetime :release_date
+      t.float :base_price
 
       t.timestamps
     end
