9fdf27a8d0a8863b3452571122a88cce23007be9
Ryan Sprott <rdsprott@gmail.com>
2018-05-07 15:01:59 -0400

    Added validation to models.

--- app/models/format.rb
+++ app/models/format.rb
@@ -1,3 +1,13 @@
 class Format < ApplicationRecord
   has_one :book
+  validate :correct_name
+
+  protected
+
+  def correct_name
+    unless ['Digital', 'Hardcover', 'Softcover'].include? name
+      errors.add(:name, 'invalid - must be Digital, Hardcover, or Softcover')
+    end
+  end
+
 end
