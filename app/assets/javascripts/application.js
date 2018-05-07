2d6229ce148ee0b86b19152d653e878affce7789
Ryan Sprott <rdsprott@gmail.com>
2018-05-07 16:05:00 -0400

    Added jQuery.  Installed Foundation.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,13 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
-//= require turbolinks
+//= require jquery3
+//= require jquery_ujs
+//= require foundation
 //= require_tree .
+
+$(function(){ 
+  // polyfill for jquery 3 - see https://stackoverflow.com/a/41262100
+  jQuery.fn.load = function(callback){ $(window).on("load", callback) };
+  $(document).foundation(); 
+});
